d49c03234f0568bbef967168f799488d86b57376
Pain-sama <145512477+pa1n-sama@users.noreply.github.com>
2024-06-04 19:58:29 +0100

    whole playlist download fixed

--- YVD_4.py
+++ YVD_4.py
@@ -18,6 +18,10 @@ if __name__=='__main__':
     urls = input('Input the videos url: ').split(',')
     check = input('Video or Audio(V/A): ')
     for i in urls:
+        if '&list' in i:
+            playlist_check=input('This url is a playlist url, do you want to download the whole playlist(Y/N)?: ')
+            if playlist_check.lower() in 'no':
+                i = i.split('&')[0]
         file_name = YouTube(i).title.strip('/.(){}[]\\')
         if check.lower() in 'video':
             download_video(i,f'/home/pain/Downloads/Videos/{file_name}','mp4')
